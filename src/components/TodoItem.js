7a9a1ca1f99e290171be584f630a590e99813a23
DamonLC21 <chiversdamon@gmail.com>
2020-06-04 16:27:40 -0400

    todo app pt. 1 - todos are showing up on the page

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TodoItem({title,content, urgent}) {
+
+    const isUrgent = () => urgent ? "todo-item urgent" : "todo-item"
+
+    return (
+        <li className={isUrgent()}>
+            <h3>{title}</h3>
+            <p>{content}</p>
+        </li>
+    )
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+export default function TodoList({todos}){
+
+    const showTodos = todos.map(todo => <TodoItem key={todo.id} {...todo}/>)
+
+    return (
+        <>
+            <h2>Todo List</h2>
+            <ul className="todo-list">
+                {showTodos}
+            </ul>
+        </>
+    )
+}
